357718ffe392a54d0365159dffe4b6fc565ac551
caojing <caojing@wdai.com>
2018-12-26 16:58:10 +0800

    model query file generate plugin

--- src/main/java/studio/rollinrock/mybatis/plugins/AdditionalQueryJavaFilePlugin.java
+++ src/main/java/studio/rollinrock/mybatis/plugins/AdditionalQueryJavaFilePlugin.java
@@ -1,8 +1,19 @@
 package studio.rollinrock.mybatis.plugins;
 
+import org.mybatis.generator.api.FullyQualifiedTable;
+import org.mybatis.generator.api.GeneratedJavaFile;
+import org.mybatis.generator.api.IntrospectedTable;
 import org.mybatis.generator.api.PluginAdapter;
+import org.mybatis.generator.api.dom.java.Field;
+import org.mybatis.generator.api.dom.java.FullyQualifiedJavaType;
+import org.mybatis.generator.api.dom.java.JavaVisibility;
+import org.mybatis.generator.api.dom.java.TopLevelClass;
+import org.mybatis.generator.config.PropertyRegistry;
+import org.mybatis.generator.internal.util.StringUtility;
 
+import java.util.Arrays;
 import java.util.List;
+import java.util.Properties;
 
 /**
  * @author rollinrock gilbombin
@@ -11,10 +22,65 @@ import java.util.List;
  * @description
  **/
 public class AdditionalQueryJavaFilePlugin extends PluginAdapter {
+
+    private String targetProject;
+//    private String targetPackage;
+    private String queryFileNameSuffix;
+    private boolean preferExtensionOverGenerics = true;
+
+    @Override
+    public void setProperties(Properties properties) {
+        super.setProperties(properties);
+        targetProject = properties.getProperty("targetProject",
+                context.getJavaClientGeneratorConfiguration().getTargetProject()); //$NON-NLS-1$
+//        targetPackage = properties.getProperty("targetPackage"); //$NON-NLS-1$
+        queryFileNameSuffix = properties.getProperty("queryFileNameSuffix", "Query");
+    }
+
     @Override
     public boolean validate(List<String> warnings) {
-        return false;
+//        if (!StringUtility.stringHasValue(targetProject)
+//                /*|| !StringUtility.stringHasValue(targetPackage)*/)
+//            return false;
+        return true;
     }
+
+    @Override
+    public List<GeneratedJavaFile> contextGenerateAdditionalJavaFiles(
+            IntrospectedTable introspectedTable) {
+        if (!preferExtensionOverGenerics) return null;
+
+        FullyQualifiedJavaType queryType =
+                new FullyQualifiedJavaType(introspectedTable.getBaseRecordType().concat(queryFileNameSuffix));
+        TopLevelClass topLevelClass = new TopLevelClass(queryType);
+        topLevelClass.setVisibility(JavaVisibility.PUBLIC);
+
+        topLevelClass.setSuperClass(introspectedTable.getBaseRecordType());
+        topLevelClass.addImportedType(introspectedTable.getBaseRecordType());
+
+        topLevelClass.addImportedType(
+                new FullyQualifiedJavaType("lombok.Data")); //$NON-NLS-1$
+        topLevelClass.addAnnotation("@Data"); //$NON-NLS-1$
+
+        Field pageIndexField = new Field();
+        pageIndexField.setVisibility(JavaVisibility.PRIVATE);
+        pageIndexField.setType(new FullyQualifiedJavaType("java.lang.Integer"));
+        pageIndexField.setName("pageIndex"); //$NON-NLS-1$
+        topLevelClass.addField(pageIndexField);
+
+        Field pageSizeField = new Field();
+        pageSizeField.setVisibility(JavaVisibility.PRIVATE);
+        pageSizeField.setType(new FullyQualifiedJavaType("java.lang.Integer"));
+        pageSizeField.setName("pageSize"); //$NON-NLS-1$
+        topLevelClass.addField(pageSizeField);
+
+        GeneratedJavaFile queryFile = new GeneratedJavaFile(topLevelClass, targetProject,
+                context.getProperty(PropertyRegistry.CONTEXT_JAVA_FILE_ENCODING),
+                context.getJavaFormatter());
+        return Arrays.asList(queryFile);
+    }
+
+
 
 
 }
--- src/test/java/studio/rollinrock/mybatis3/model/PointsMallV2Banner.java
+++ src/test/java/studio/rollinrock/mybatis3/model/PointsMallV2Banner.java
@@ -1,46 +1,31 @@
 package studio.rollinrock.mybatis3.model;
 
-import io.swagger.annotations.ApiModel;
-import io.swagger.annotations.ApiModelProperty;
 import java.util.Date;
 import lombok.Data;
 
 @Data
-@ApiModel
 public class PointsMallV2Banner {
-    @ApiModelProperty(name="id",notes="主键")
     private Integer id;
 
-    @ApiModelProperty(name="deleted",notes="是否删除：0-未删除; 1-已删除")
     private Boolean deleted;
 
-    @ApiModelProperty(name="creator",notes="创建人（对应登录名）")
     private String creator;
 
-    @ApiModelProperty(name="gmtCreate",notes="创建时间")
     private Date gmtCreate;
 
-    @ApiModelProperty(name="modifier",notes="修改人（对应登录名）")
     private String modifier;
 
-    @ApiModelProperty(name="gmtModified",notes="修改时间")
     private Date gmtModified;
 
-    @ApiModelProperty(name="title",notes="名称")
     private String title;
 
-    @ApiModelProperty(name="picUrl",notes="banner图片url")
     private String picUrl;
 
-    @ApiModelProperty(name="destination",notes="banner目的地：普通banner存url，商品banner存商品id")
     private String destination;
 
-    @ApiModelProperty(name="enabled",notes="是否启用")
     private Boolean enabled;
 
-    @ApiModelProperty(name="rank",notes="排序号")
     private Integer rank;
 
-    @ApiModelProperty(name="style",notes="0-普通banner，1-商品banner")
     private Byte style;
 }
